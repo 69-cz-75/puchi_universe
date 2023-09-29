1ca66d14ec6a4b52a111202b6fa2ae9cc67a48ba
shiho <epicurean.love.69@gmail.com>
2023-09-29 12:31:36 +0900

    add:ハンバーガーアイコンのテストの追加 #17

--- spec/components/header_component_spec.rb
+++ spec/components/header_component_spec.rb
@@ -15,4 +15,18 @@ RSpec.describe HeaderComponent, type: :component do
       expect(page).to have_css("button.btn.btn-ghost svg[h-5][w-5]")
     end
   end
+
+  it "ハンバーガーアイコンの表示" do
+    render_inline(HeaderComponent.new(header: 'Myheader'))
+
+    # ハンバーガーアイコンが存在することをテスト
+    expect(page).to have_selector("svg[xmlns*='http://www.w3.org/2000/svg']")
+
+    # パス要素が存在することをテスト
+    expect(page).to have_css("path[stroke-linecap='round'][stroke-linejoin='round'][stroke-width='2']")
+
+    # パスの属性や形状をテストするには、正確なパスデータを指定してテストします
+    # パスデータは "d" 属性にあります
+    expect(page).to have_css("path[d='M4 6h16M4 12h16M4 18h16']")
+  end
 end
