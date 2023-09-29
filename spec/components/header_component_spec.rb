b5dc87c63fb7f77cf23ce5c370c540490d01275d
shiho <epicurean.love.69@gmail.com>
2023-09-29 11:32:30 +0900

    add:検索アイコンのテストの追加#17

--- spec/components/header_component_spec.rb
+++ spec/components/header_component_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe HeaderComponent, type: :component do
     expect(page).to have_selector("img[alt='header_logo']")
   end
 
+  it "検索アイコンの表示" do
+    within ".navbar-end" do
+      expect(page).to have_css("button.btn.btn-ghost")
+      expect(page).to have_css("button.btn.btn-ghost svg[h-5][w-5]")
+    end
+  end
 end
