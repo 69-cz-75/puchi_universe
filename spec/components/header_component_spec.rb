4ec30dcd66fba518946d98a31c24fdcabbb64c41
shiho <epicurean.love.69@gmail.com>
2023-10-01 11:45:14 +0900

    fix:expect(page)をexpect(rendered_content)に変更#17

--- spec/components/header_component_spec.rb
+++ spec/components/header_component_spec.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
+#[https://viewcomponent.org/CHANGELOG.html#:~:text=Reegan%20Viljoen-,v3.0.0,-1%2C000%2B%20days%20and]
+#BREAKING: Remove deprecated rendered_component in favor of rendered_content.
+
 require "rails_helper"
 
 RSpec.describe HeaderComponent, type: :component do
   it "ロゴの表示" do
     render_inline(HeaderComponent.new(header: 'Myheader'))
 
-    expect(page).to have_selector("img[alt='header_logo']")
+    expect(rendered_content).to have_selector("img[alt='header_logo']")
   end
 
   it "検索アイコンの表示" do
     within ".navbar-end" do
-      expect(page).to have_css("button.btn.btn-ghost")
-      expect(page).to have_css("button.btn.btn-ghost svg[h-5][w-5]")
+      expect(rendered_content).to have_css("button.btn.btn-ghost")
+      expect(rendered_content).to have_css("button.btn.btn-ghost svg[h-5][w-5]")
     end
   end
 
@@ -20,13 +23,13 @@ RSpec.describe HeaderComponent, type: :component do
     render_inline(HeaderComponent.new(header: 'Myheader'))
 
     # ハンバーガーアイコンが存在することをテスト
-    expect(page).to have_selector("svg[xmlns*='http://www.w3.org/2000/svg']")
+    expect(rendered_content).to have_selector("svg[xmlns*='http://www.w3.org/2000/svg']")
 
     # パス要素が存在することをテスト
-    expect(page).to have_css("path[stroke-linecap='round'][stroke-linejoin='round'][stroke-width='2']")
+    expect(rendered_content).to have_css("path[stroke-linecap='round'][stroke-linejoin='round'][stroke-width='2']")
 
     # パスの属性や形状をテストするには、正確なパスデータを指定してテストします
     # パスデータは "d" 属性にあります
-    expect(page).to have_css("path[d='M4 6h16M4 12h16M4 18h16']")
+    expect(rendered_content).to have_css("path[d='M4 6h16M4 12h16M4 18h16']")
   end
 end
