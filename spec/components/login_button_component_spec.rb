d04270a8a7a082f6d8952c09d3856249902f644a
shiho <epicurean.love.69@gmail.com>
2023-10-17 21:02:43 +0900

    add:loginボタンとsignupボタンの作成#23

---
+++ spec/components/login_button_component_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe LoginButtonComponent, type: :component do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
+  #   ).to include(
+  #     "Hello, components!"
+  #   )
+  # end
+end
