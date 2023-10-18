57fda440513f21ed616599a73dbdb5f0fa7833be
shiho <epicurean.love.69@gmail.com>
2023-10-18 11:52:33 +0900

    fix:header_componentとfooter_componentを削除しパーシャルで再度作成し直す。それに伴い調整を行う#23

--- app/components/header_component.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class HeaderComponent < ViewComponent::Base
-  def initialize(header:)
-    @header = header
-  end
-
-end
--- app/components/userlogin_link_component.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class UserloginLinkComponent < ViewComponent::Base
-  def initialize(header_link:)
-    @header_link = header_link
-  end
-
-  
-end
