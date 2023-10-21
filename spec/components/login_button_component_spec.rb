a53ea76d64ea6836836152223f81f1f7991ac853
shiho <epicurean.love.69@gmail.com>
2023-10-21 22:39:46 +0900

    fix:login_button_componentとsignup_button_componentのテストを日本語化に修正#25

--- spec/components/login_button_component_spec.rb
+++ spec/components/login_button_component_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe LoginButtonComponent, type: :component do
   it "ログインボタンの表示" do
     render_inline(LoginButtonComponent.new(loginbutton: 'Login in'))
 
-    expect(rendered_content).to have_css('a.btn.btn-active.btn-ghost', text: 'Log in')
+    expect(rendered_content).to have_css('a.btn.btn-active.btn-ghost', text: I18n.t('defaults.login'))
   end
 end
