69cf6b35a367d741d5c0105011f5af0a4d27c174
shiho <epicurean.love.69@gmail.com>
2023-11-06 23:02:32 +0900

    add:galleryの一覧画面にコンテンツがない場合の表示を追加#29

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,4 +1,5 @@
 class GalleriesController < ApplicationController
   def index
+    @scene_photos = ScenePhoto.all.includes(:user).order(created_at: :desc)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
     registrations:  'users/registrations'
   }
 
+  resources :galleries, only: %i[index]
 end
