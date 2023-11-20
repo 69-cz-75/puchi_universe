adc5f035b5a88a02ee511811d4361559a0ef8ab5
shiho <epicurean.love.69@gmail.com>
2023-11-20 22:49:36 +0900

    add:投稿ボタンを押下時、タイトルが空欄の場合は自動で無題というタイトルが挿入されるように調整#33

--- app/controllers/scene_photos_controller.rb
+++ app/controllers/scene_photos_controller.rb
@@ -8,7 +8,10 @@ class ScenePhotosController < ApplicationController
   end
 
   def create
-    @scene_photo = current_user.scene_photos.build(scene_photo_params)
+    #投稿するボタンを押下した際にタイトルがnilまたは空の時は自動でuntitledを挿入する
+    title = scene_photo_params[:title]
+    title = I18n.t('scene_photos.untitled') if title.nil? || title.empty?
+    @scene_photo = current_user.scene_photos.build(scene_photo_params.merge(title: title))
     if @scene_photo.save
       redirect_to galleries_path
     else
--- app/models/scene_photo.rb
+++ app/models/scene_photo.rb
@@ -4,11 +4,4 @@ class ScenePhoto < ApplicationRecord
   validates :title, presence: true, length: { maximum: 32 }
   validates :caption, length: { maximum: 3000 }
 
-  before_validation :set_default_title
-
-  private
-
-  def set_default_title
-    self.title = 'Untitled' if title.blank?
-  end
 end
