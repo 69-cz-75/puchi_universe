0fab3b6ab18370f64623a2a0b2c555064cb9b929
shiho <epicurean.love.69@gmail.com>
2023-11-27 22:00:23 +0900

    add:投稿ページを作成。手動での投稿を確認#38

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/galleries/new', to: 'scene_photos#new'
   post '/galleries/create', to: 'scene_photos#create'
   get '/museums', to: 'scale_comparison_photos#index', as: 'museums'
+  get '/museums/new', to: 'scale_comparison_photos#new'
+  post '/museums/create', to: 'scale_comparison_photos#create'
 
   get 'terms_of_service', to: 'static_pages#terms_of_service'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
