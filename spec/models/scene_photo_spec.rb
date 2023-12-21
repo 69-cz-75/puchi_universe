979e04525e362e0a47a2c1df6f204ee532ac69ac
shiho <epicurean.love.69@gmail.com>
2023-12-21 21:53:03 +0900

    add:Tagのテストを追加#57

--- spec/models/scene_photo_spec.rb
+++ spec/models/scene_photo_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe ScenePhoto, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "タグの操作" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:scene_photo) { FactoryBot.build(:scene_photo, user: user, images: nil) }
+
+    before do
+      # 画像ファイルを添付する（CarrierWaveを使用する場合）
+      image_path = Rails.root.join('spec', 'fixtures', 'test_image1.jpg')
+      scene_photo.images = [File.open(image_path)]
+
+      scene_photo.save!
+    end
+
+    it "複数のタグを持つこと" do
+      tag1 = FactoryBot.create(:tag)
+      tag2 = FactoryBot.create(:tag)
+
+      scene_photo.tags << tag1
+      scene_photo.tags << tag2
+
+      expect(scene_photo.tags.count).to eq(2)
+    end
+
+    it "タグを削除することができる" do
+      tag1 = FactoryBot.create(:tag)
+      tag2 = FactoryBot.create(:tag)
+
+      scene_photo.tags << tag1
+      scene_photo.tags << tag2
+
+      expect(scene_photo.tags.count).to eq(2)
+
+      tag_to_remove = scene_photo.tags.first
+      scene_photo.tags.destroy(tag_to_remove)
+
+      expect(scene_photo.tags.count).to eq(1)
+    end
+  end
 end
