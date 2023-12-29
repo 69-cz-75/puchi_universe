f1ad54c8c108ea2e5197d76119a3f3b1ded93bfd
shiho <epicurean.love.69@gmail.com>
2023-12-29 19:55:53 +0900

    add:profileのモデルテストの追加#57

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "プロフィールの登録ができる" do
+    user = User.create(email: "test@example.com", password: "password")
+    profile = described_class.new(name: "John", gender: :male, birthdate: Date.new(1990, 1, 1), user: user)
+    expect(profile).to be_valid
+  end
+
+  it "名前が未入力だと登録できない" do
+    profile = described_class.new(gender: :male, birthdate: Date.new(1990, 1, 1))
+    expect(profile).to_not be_valid
+  end
+
+  it "性別が未入力だと登録できない" do
+    profile = described_class.new(name: "John", birthdate: Date.new(1990, 1, 1))
+    expect(profile).to_not be_valid
+  end
+
+  it "誕生日が未入力だと登録できない" do
+    profile = described_class.new(name: "John", gender: :male)
+    expect(profile).to_not be_valid
+  end
 end
