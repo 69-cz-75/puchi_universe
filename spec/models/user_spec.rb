8639ebab3955317f22af57edf87bbac3fe327f89
shiho <epicurean.love.69@gmail.com>
2023-12-31 19:51:08 +0900

    add:userのモデルとシステムテストの追加#57

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid  # user.valid? が true になればパスする
     end
   end
+
+  describe 'ログイン' do
+    it "正しいメールアドレスとパスワードでログインできる" do
+      user = create(:user, email: 'test@example.com', password: 'password123')
+
+      authenticated_user = User.find_for_authentication(email: 'test@example.com')
+
+      expect(authenticated_user.valid_password?('password123')).to be true
+    end
+  end
 end
